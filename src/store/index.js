afef58d0d99a80b5fd6612702445b56035694b87
uniyalnitin <nitin.eee14@nituk.ac.in>
2020-06-30 23:07:15 +0530

    add store

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import reducer from '../reducers';
+
+let store;
+
+export function configureStore() {
+  store = createStore(reducer, applyMiddleware(logger, thunk));
+  return store;
+}
