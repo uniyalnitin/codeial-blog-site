972407c4be9aa6046c14908454f47b6282c5acc9
uniyalnitin <nitin.eee14@nituk.ac.in>
2020-07-12 20:45:33 +0530

    FriendsList component UI updated

--- src/components/FriendListItem.js
+++ src/components/FriendListItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function FriendListItem(props) {
-  const { friend } = this.props;
+function FriendsListItem(props) {
   return (
-    <div className="friends-item">
-      <div className="friends-name">{friend.name}</div>
+    <div>
+      <Link className="friends-item" to={`user/${props.friend._id}`}>
+        <div className="friends-img">
+          <img
+            src="https://image.flaticon.com/icons/svg/2154/2154651.svg"
+            alt="user-pic"
+          />
+        </div>
+        <div className="friends-name">{props.friend.email}</div>
+      </Link>
     </div>
   );
 }
 
-export default FriendListItem;
+export default FriendsListItem;
