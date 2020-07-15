8048d9393ebe1bdd73f83cb9af65efe81b5b61e9
uniyalnitin <nitin.eee14@nituk.ac.in>
2020-07-15 11:44:21 +0530

    Creating Post: clear the textarea on submit

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -13,6 +13,9 @@ class CreatePost extends Component {
   handleOnClick = () => {
     //dispatch action to create post
     this.props.dispatch(createPost(this.state.content));
+    this.setState({
+      content: '',
+    });
   };
 
   handleChange = (e) => {
